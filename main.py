37d129b3a2574a94343e5e0040b37099e70d147e
Sandro Meireles <sandro.meirelesdev@gmail.com>
2023-03-14 09:26:31 -0300

    feat: Create base entities

---
+++ main.py
@@ -0,0 +1,33 @@
+class Book:
+    pass
+
+
+class Client:
+    pass
+
+
+class Attendant:
+    pass
+
+
+class Database:
+
+    books = []
+    clients = []
+
+    def create_client():
+        pass
+
+    def create_book():
+        pass
+
+    def create_attendenant():
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
